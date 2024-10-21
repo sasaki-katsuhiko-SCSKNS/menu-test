551b0ad7f881ab902762d5f17dc87adb2775ab5a
sasaki-katsuhiko-SCSKNS <sasakikats@yamaha-motor.co.jp>
2024-10-21 14:34:49 +0900

    202410-21_3

---
+++ src/views/yGroupManagement.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useLockScreen } from 'viy-ui';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+const { lockScreen } = useLockScreen();
+
+defineOptions({
+  name: 'yGroupManagement',
+});
+
+const form = ref();
+
+const formData = reactive({
+});
+</script>
+
+<template>
+  <VueForm ref="form" v-loading="lockScreen" :model="formData">
+    <VueText id="viy2Text_exLAO" ref="viy2Text_exLAO" :style="{ width: '100%', display: 'inline-block' }">
+      グループ管理
+    </VueText>
+  </VueForm>
+</template>
