3c84ea654b23616f0c98768292a22007a95ab710
sasaki-katsuhiko-SCSKNS <sasakikats@yamaha-motor.co.jp>
2024-10-22 10:51:01 +0900

    Add files via upload

---
+++ dist-module/index.js
@@ -0,0 +1,16 @@
+import "./style.css";
+window.__APP_YNA_G3_FRONT_BLANK__ = Object.assign(window.__APP_YNA_G3_FRONT_BLANK__ || {}, { pkg: { devDependencies: { "@vitejs/plugin-vue": "^5.1.4", "@vitejs/plugin-vue-jsx": "^4.0.1", "@vue/test-utils": "^2.4.6", "@vueuse/core": "^11.1.0", "@ymc-group/eslint-config": "^1.0.1", "@ymc-group/plugins-userauthorize": "1.0.1", "@ymc-group/preview-server": "^1.0.19", "@ymc-group/vite-config": "^1.0.2", "@ymc-group/viy-menu": "^2.1.1", "@ymc-group/viy-ui": "^2.1.0", axios: "^1.7.7", dayjs: "^1.11.13", echarts: "^5.5.1", exceljs: "^4.4.0", jsdom: "^25.0.1", lightningcss: "^1.27.0", "lodash-es": "^4.17.21", pinia: "^2.2.4", "unplugin-auto-import": "^0.18.3", vite: "^5.4.8", "vite-svg-loader": "^5.1.0", vitest: "^2.1.2", vue: "^3.5.11", "vue-hooks-plus": "^2.2.1", "vue-i18n": "^10.0.4", "vue-router": "^4.4.5" }, name: "yna-g3-front-blank", version: "0.0.0" }, lastBuildTime: "2024-10-21T08:21:30.038Z" });
+const e = {}, i = {}, s = {}, n = /* @__PURE__ */ Object.assign({}), u = { en: e, ja: i, zh: s };
+function t() {
+  return {
+    init() {
+      return {
+        views: n,
+        locales: u
+      };
+    }
+  };
+}
+export {
+  t as default
+};
